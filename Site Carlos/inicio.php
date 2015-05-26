6d7ab4cb4f42b57f7c569e45da25a66bc0901dd4
tuxokalsing <tuxokalsing@gmail.com>
2015-05-26 16:41:03 -0300

    mudança de nomes

--- Site Carlos/inicio.php
+++ Site Carlos/inicio.php
@@ -1,9 +1,9 @@
-﻿<?php
+<?php
 /*
-*	Website Pessoal Carlos Gutterres
+*	Universidade Federal do Rio Grande do Sul - Centro de Biotecnologia
 *
 *	Desenvolvedores: 	Arthur Kalsing
-*	Descrição: 			Página inicial website (fotos, apresentação, links para sessões)
+*	Descrição: 			Página inicial
 *
 */		
 
@@ -11,123 +11,243 @@
 
 <!DOCTYPE html>
 <html lang="pt">
-<head>
-	<title>Carlos Gutterres</title>
-	<meta charset="utf-8">
-	<link type="text/css" rel="stylesheet" href="styles/style.css" />
-	<link type="text/css" rel="stylesheet" href="styles/skin.css" />
-	<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script>
-	<script type="text/javascript" src="js/jquery.jcarousel.min.js"></script>
-	<script type="text/javascript" src="js/initSlider.js"></script>
-	<link rel="stylesheet" type="text/css" href="css/common.css" />
-	<link rel="stylesheet" type="text/css" href="css/style2.css" />
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,700' rel='stylesheet' type='text/css' />
-	<script type="text/javascript" src="js/modernizr.custom.79639.js"></script> 
-	<!--[if lte IE 8]><style>.main{display:none;} .support-note .note-ie{display:block;}</style><![endif]-->
-	
-	
-	<!--[if IE 6]>
-	<script src="js/ie6-transparency.js"></script>
-	<script>DD_belatedPNG.fix('#header img, #featured-section h2, #circles img, #frontpage-sidebar .read-more, .blue-bullets li, #sidebar .sidebar-button, #project-content .read-more, .more-link, #contact-form .submit, .jcarousel-skin-tango .jcarousel-next-horizontal, .jcarousel-skin-tango .jcarousel-prev-horizontal, #commentform .submit');</script>
-	<style>body { behavior: url("styles/ie6-hover-fix.htc"); }</style>
-	<link rel="stylesheet" href="styles/ie6.css" />
-	<![endif]-->
-	<!--[if IE 7]><link rel="stylesheet" href="styles/ie7.css" /><![endif]-->
-	<!--[if IE 8]><link rel="stylesheet" href="styles/ie8.css" /><![endif]-->
-</head>
-<body class="home">
-<div id="wrap">
-<!--==============================header=================================-->  
-  <div id="header" style="padding-top: 20px">
-  <header> 
-	<?php include("header.php"); ?>
-  </header>
-  </div>
-<!--==============================Content=================================-->
-  <div id="featured-section">
-  
-	<ul class="ch-grid">
-		<li>
-			<div class="ch-item ch-img-1">
-				<div class="ch-info">
-					<h3>Carlos</h3>
-					<p>Descubra <a href="#">aqui</a></p>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="ch-item ch-img-2">
-				<div class="ch-info">
-					<h3>Vivessencias</h3>
-					<p>Descubra <a href="#">aqui</a></p>
-				</div>
-			</div>
-		</li>
-		<li>
-			<div class="ch-item ch-img-3">
-				<div class="ch-info">
-					<h3>Espiri
-					tualidade</h3>
-					<p>Descubra <a href="#">aqui</a></p>
-				</div>
-			</div>
-		</li>
+	<head>
+		<title>Carlos Gutterres - Portfólio</title>
+		<meta name="description" content="Incubadora Empresarial do Centro Biotecnologia da UFRGS, IECBiot">
+		<meta name="keywords" content="iecbiot, incubadora, empreendimentos, edital, incubação, pesquisa, desenvolvimento, pdi, biotecnologia"/>
+		<meta charset="utf-8">
+		<meta name="format-detection" content="telephone=no" />
+		<link rel="icon" type="image/gif" href="images/favicon.gif">
 		
-		<li>
-			<div class="ch-item ch-img-4">
-				<div class="ch-info">
-					<h3>Jin Shin Jyutsu</h3>
-					<p>Descubra <a href="#">aqui</a></p>
+		<link rel="stylesheet" type="text/css" href="css/menu_circles.css" />
+		
+		<link rel="shortcut icon" href="images/favicon.gif" />
+		<link rel="stylesheet" href="css/ie/style_common.css" />
+		<link rel="stylesheet" href="css/ie/style4.css" />
+		<link rel="stylesheet" href="css/ie/camera.css">
+		<link rel="stylesheet" href="css/ie/style.css">
+		<link rel="stylesheet" href="css/ie/allery.css">
+		
+		<script src="js/ie/jquery.js"></script>
+		<script src="js/ie/jquery-migrate-1.2.1.js"></script>
+		<script src="js/ie/script.js"></script>
+		<script src="js/ie/superfish.js"></script>
+		<script src="js/ie/jquery.ui.totop.js"></script>
+		<script src="js/ie/jquery.equalheights.js"></script>
+		<script src="js/ie/jquery.mobilemenu.js"></script>
+		<script src="js/ie/camera.js"></script>
+		<!--[if (gt IE 9)|!(IE)]><!-->
+		<script src="js/ie/jquery.mobile.customized.min.js"></script>
+		<!--<![endif]-->
+		<script>
+			$(document).ready(function(){
+				jQuery('#camera_wrap').camera({
+					loader: false,
+					pagination: false ,
+					minHeight: '444',
+					thumbnails: false,
+					height: '27.86458333333333%',
+					caption: true,
+					navigation: true,
+					fx: 'simpleFade'
+				});
+				$().UItoTop({ easingType: 'easeOutQuart' });
+			});
+		</script>
+		<!--[if lt IE 8]>
+			<div style=' clear: both; text-align:center; position: relative;'>
+				<a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode">
+					<img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." />
+				</a>
+			</div>
+		<![endif]-->
+		<!--[if lt IE 9]>
+		<script src="js/html5shiv.js"></script>
+		<link rel="stylesheet" media="screen" href="css/ie.css">
+		<![endif]-->		
+	</head>
+	<body id="top" class="fundo_1">
+		<div class="main">
+<!--==============================header=================================-->
+			<header class="menu_sup fundo_menu_1">
+				<div class="container_12">
+					<div class="row">
+					<div class="grid_4">
+						<div class="titulo texto_menu_1">- Terapeuta vibracional, ocultista e neo-alquimista -</div>
+						<div class="nome texto_menu_1">CARLOS GUTTERRES</div>
+					</div>
+					<div class="grid_8">
+						<ul class="ch-grid">
+							<li>
+								<div class="ch-item ch-img-1">
+									<div class="ch-info">
+										<h3 class="texto_menu_1">Carlos</h3>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="ch-item ch-img-2">
+									<div class="ch-info">
+										<h3 class="texto_menu_1">Vivessencias</h3>
+									</div>
+								</div>
+							</li>
+							<li>
+								<div class="ch-item ch-img-3">
+									<div class="ch-info">
+										<h3 class="texto_menu_1">Espiritualidade</h3>
+									</div>
+								</div>
+							</li>
+							
+							<li>
+								<div class="ch-item ch-img-4">
+									<div class="ch-info">
+										<h3 class="texto_menu_1">Jin Shin Jyutsu</h3>
+									</div>
+								</div>
+							</li>
+						</ul>
+					</div>
+					</div>
+				</div>
+			</header>
+<!--==============================Content=================================-->
+			<div class="hor_separator" style="padding: 0px;"></div>
+			<div class="content corpo_1">
+				<div class="container_12">
+					<div class="grid_7">
+						<h3>Biografia</h3>
+						
+						<p>Terapeuta vibracional, ocultista e neo-alquimista vem dedicando-se à pesquisa dos temas relacionados à espiritualidade a mais quarenta anos. Quando jovem, o seu grande interesse pelos mistérios da natureza e do ser humano, levou-o ao estudo de: filosofia, psicologia, numerologia, astrologia, cabala, ocultismo, alquimia e todas as formas de expressão da sabedoria antiga. Foi afortunado com a oportunidade de aprender com dois grandes expoentes desta área: Manoel Wilson Tavares Gutterres e Domingos Rubbo, de quem recebeu orientação e ensinamentos por décadas. Participou de diversas entidades espiritualistas e foi idealizador e fundador da Ordo Phoenicis, Escola de Estudos Herméticos. Também foi um dos fundadores da Loja Maçônica Perseu n° 84 e presidente da FEEU.</p>
+						<p>No início dos anos 90 sentiu necessidade de um canal de expressão prática para tudo aquilo que vinha estudando. Encontrou-o nas artes da cura vibracional. Primeiro conheceu a terapia floral, na qual tornou-se um estudioso, pesquisador e terapeuta. Logo a seguir foi introduzido ao Jin Shin Jyutsu, reconhecendo nele, juntamente com os Florais o caminho que tanto procurava, onde integrou toda a sua vida de estudos numa estrutura perfeita de expressão.</p>
+						<p>Pesquisador e co-criador das essências Vibracionais Vega e do Sistema Vida, sistemas de essências desenvolvidas a partir de 1995 com flores da região sul do Brasil, além de essências Ambientais, Elixires Minerais, Fórmulas Compostas, e essências Energéticas (foi pioneiro na idealização destas últimas). Também é Mestre Reiki e Instrutor autorizado pela Jin Shin Jyutsu Inc. Desde então dedica-se à pratica, ensino, pesquisa e divulgação destes conhecimentos.</p>
+
+					</div>
+					<div class="grid_4 prefix_1">
+						<h3>Bibliografia</h3>
+						<div class="news">
+							<p class="color1 destaque" ><a href="noticias.php#2015-09-04">Lançamento do livro
+							O Caminho Luminoso da Paz</a> 
+							<a href="http://sgi.macropus.com.br/iecbiot/edital/" class="color1 destaque" target="_blank">PARTICIPE!</a>
+							</p>
+						</div>
+						<div class="news">
+							<time datetime="2015-09-04">09<span>ABR</span></time>
+							<div class="extra_wrapper">
+								<p class="color1"><a href="noticias.php#2015-09-04">Workshop CANVAS</a></p>
+								Foi realizado no dia 07/04 o Workshop CANVAS, a primeira etapa da Usina do Empreendedor. 
+								Os participantes confeccionaram o modelo Canvas de seus próprios empreendimentos.
+							</div>
+						</div>
+						<div class="news">
+							<time datetime="2015-13-03">13<span>MAR</span></time>
+							<div class="extra_wrapper">
+								<p class="color1"><a href="noticias.php#2015-13-03">Usina do Empreendedor IECBiot </a></p>
+								A IECBiot lançara Usina do Empreendedor, uma ação dentro de seu programa de empreendedorismo que 
+								colocara os participantes em contato com o mundo do empreendedor.
+							</div>
+						</div>
+					</div>
+					<!--
+					<div class="clear"></div>
+					<div class="grid_12">
+						<div class="hor_separator"></div>
+						<h3 class="head1 center">Venha nos conhecer!</h3>
+					</div>
+					<div class="clear"></div>
+					<div class="clear"></div>
+					<div class="grid_12">
+						<center>
+						<div class="grid_4 index_highlights"> 
+							<div id="index_photo_container" style="background-image: url(images/inicial_1.jpg);">
+								<div class="index_photo_par1">
+									<div class="index_photo_par2">
+										<div class="index_photo_par3">
+											Referencia em Biotecnologia
+										</div>
+									</div>
+								</div>
+							</div> 
+						</div>
+						<div class="grid_4 index_highlights">
+							<div id="index_photo_container" style="background-image: url(images/inicial_2.jpg);">
+								<div class="index_photo_par1">
+									<div class="index_photo_par2">
+										<div class="index_photo_par3">
+											O caminho para o sucesso!
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="grid_4 index_highlights">
+							<div id="index_photo_container" style="background-image: url(images/inicial_3.jpg);">
+								<div class="index_photo_par1">
+									<div class="index_photo_par2">
+										<div class="index_photo_par3">
+											Venha nos conhecer!
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						</center>
+					</div>
+					<div class="clear"></div>
+					<div class="grid_12">
+						<div class="hor_separator"></div>
+						<h3 class="head1 center">Nossos Parceiros</h3>
+					</div>
+					<div class="grid_3">
+						<div class="box">
+							<div class="box_title">ANPROTEC</div>
+							<div class="box_bot">
+								<div class="maxheight">
+									<a href="http://www.anprotec.org.br/" target="_blank"><img class="parceiros" src="images/logo_anprotec.jpg" alt=""></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="grid_3">
+						<div class="box">
+							<div class="box_title">SEBRAE</div>
+							<div class="box_bot">
+								<div class="maxheight">
+									<a href="http://www.sebrae.com.br/" target="_blank"><img class="parceiros" src="images/logo_sebrae.jpg" alt=""></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="grid_3">
+						<div class="box">
+							<div class="box_title">FINEP</div>
+							<div class="box_bot">
+								<div class="maxheight">
+									<a href="http://www.finep.gov.br/" target="_blank"><img class="parceiros" src="images/logo_finep.jpg" alt=""></a>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="grid_3">
+						<div class="box">
+							<div class="box_title">CNPq</div>
+							<div class="box_bot">
+								<div class="maxheight">
+									<a href="http://www.cnpq.br/" target="_blank"><img class="parceiros" src="images/logo_cnpq.jpg" alt=""></a>
+								</div>
+							</div>
+						</div>
+					</div>
+				<div class="clear"></div> -->
+				FOTOOOOS
+				
 				</div>
 			</div>
-		</li>
-	</ul>
-  
-    <!--end circles-->
-    <div id="image-slider">
-      <ul id="mycarousel" class="jcarousel-skin-tango">
-        <li class="first"><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li class="first"><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li class="first"><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-        <li><a href="#"><img width="280" height="190" src="images/280x190.gif" alt="" /></a></li>
-      </ul>
-    </div>
-    <!--end image-slider-->
-  </div>
-  <!--end featured-section-->
-  <div id="frontpage-main">
-    <div id="frontpage-content">
-      <h3>Why Work With Akono?</h3>
-      <p>You’ll not regret getting in touch with us about your new project. Below are some of the benefits.</p>
-      <ul class="blue-bullets">
-        <li>Over 15 Years Real World Experience</li>
-        <li>Voted No. 1 Design Studio In The World</li>
-        <li>Support And Guidance At Every Step</li>
-        <li>A Great Solution That You&#8217;ll Be Happy With</li>
-      </ul>
-    </div>
-    <!--end frontpage-content-->
-    <div id="frontpage-sidebar">
-      <h3>Latest Article</h3>
-      <a class="blog-title" href="#">Top Tips For Hiring A Design Agency</a>
-      <p class="meta">1st May 2011 / <a href="#">No Comments &#187;</a></p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip.</p>
-      <a class="read-more" href="#">Read More &raquo;</a> </div>
-    <!--end frontpage-sidebar-->
-  </div>
-  <!--end frontpage-main-->
-  <div id="footer">
-    <p class="copyright">Copyright &copy; <a href="#">Domain Name</a> - All Rights Reserved / Design By <a target="_blank" href="http://www.chris-creed.com/">Chris Creed</a> | Download more: <a href="http://www.csstemplatesfree.org">CSS Website Templates</a></p>
-  </div>
-  <!--end footer-->
-</div>
-<!--end wrap-->
-</body>
-<div class="cache-images"><img src="images/red-button-bg.png" width="0" height="0" alt="" /><img src="images/black-button-bg.png" width="0" height="0" alt="" /></div>
-<!--end cache-images-->
+		</div>
+<!--==============================footer=================================-->
+		<footer>
+			<?php include("footer.php"); ?>
+		</footer>
+	</body>
 </html>
